e69a4f7ee2f43cfe83289fc98e57a12129986cee
Tomi <52747351+tomiiha@users.noreply.github.com>
2023-06-27 17:24:01 +0100

    Update main.js

--- main.js
+++ main.js
@@ -15,25 +15,43 @@ async function fetchData() {
         back: card.Back,
         note: card.Note
     }));
+    shuffle(flashcards); // Shuffle the cards
     updateCard();
+}
+
+// Fisher-Yates (aka Knuth) Shuffle
+function shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
 }
 
 // Get DOM elements
 const cardContainer = document.querySelector('.card-container');
 const cardFront = document.querySelector('.card-front p');
 const cardBack = document.querySelector('.card-back p');
+const noteElement = document.getElementById('note');
 const prevButton = document.getElementById('prev');
+const showButton = document.getElementById('show');
 const nextButton = document.getElementById('next');
 
 // Function to update the card display
 function updateCard() {
     cardFront.innerText = flashcards[currentCard].front;
     cardBack.innerText = flashcards[currentCard].back;
-    
+    noteElement.innerText = 'Note: ' + flashcards[currentCard].note;
+
     // Show the front of the card and hide the back
-    cardFront.style.display = 'flex';
+    cardFront.style.display = 'block';
     cardBack.style.display = 'none';
-    
+
     // Disable or enable buttons
     prevButton.disabled = currentCard === 0;
     nextButton.disabled = currentCard === flashcards.length - 1;
@@ -45,16 +63,14 @@ prevButton.addEventListener('click', function() {
     updateCard();
 });
 
+showButton.addEventListener('click', function() {
+    cardFront.style.display = 'none';
+    cardBack.style.display = 'block';
+});
+
 nextButton.addEventListener('click', function() {
     currentCard++;
     updateCard();
-});
-
-// Add event listener to the card to flip it
-cardContainer.addEventListener('click', function() {
-    const cardFrontDisplay = cardFront.style.display;
-    cardFront.style.display = cardFrontDisplay === 'flex' ? 'none' : 'flex';
-    cardBack.style.display = cardFrontDisplay === 'flex' ? 'flex' : 'none';
 });
 
 // Fetch data from Google Sheets and set the initial display
