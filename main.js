07882064b903721b5816e66c9fd87f1c76e79322
Tomi <52747351+tomiiha@users.noreply.github.com>
2023-06-27 20:08:59 +0100

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     var show = document.getElementById('show');
 
     function getData() {
-        Papa.parse('https://YOUR_USERNAME.github.io/language_flashcards/data.csv', {
+        Papa.parse('data.csv', {
             download: true,
             header: true,
             complete: function(results) {
