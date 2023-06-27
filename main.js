aa829a54c292168e8d76fa846f5e1ef67f574dd9
Tomi <52747351+tomiiha@users.noreply.github.com>
2023-06-27 20:51:43 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,75 +1,48 @@
-$(document).ready(function() {
-    var data;
-    var currentCard = 0;
-    var card = $('.card-content');
-    var cardFront = $('.card-front');
-    var cardBack = $('.card-back');
-    var show = document.getElementById('show');
-    var next = document.getElementById('next');
-    var prev = document.getElementById('prev');
+var currentCard;
+var currentCardIndex = 0; // Define currentCardIndex variable
 
-    function getData() {
-        Papa.parse('https://tomiiha.github.io/language_flashcards/data.csv', {
-            download: true,
-            header: true,
-            complete: function(results) {
-                data = results.data;
-                console.log("Data loaded successfully. Number of entries: " + data.length);
-                shuffleArray(data);
-                showCardFront();
-            },
-            error: function(err, file, inputElem, reason) {
-                console.log("Error occurred while loading data: ", err, reason);
-            }
-        });
+function showCardFront(index) {
+    currentCard = flashcardsData[index];
+    flashcardContainer.innerHTML = currentCard['Front'];
+}
+
+function showCardBack() {
+    console.log("currentCard:", currentCard);
+
+    if (currentCard && currentCard['Back'] && currentCard['Back'].trim().length > 0) {
+        flashcardContainer.innerHTML = currentCard['Back'];
+    } else {
+        throw new Error(`Back data is not available for currentCard index ${currentCardIndex}.`); // Use currentCardIndex here
     }
+}
 
-    function shuffleArray(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
+function showNextCard() {
+    if (currentCardIndex < flashcardsData.length - 1) {
+        currentCardIndex++; // Increment currentCardIndex when showing next card
+    } else {
+        currentCardIndex = 0; // Reset currentCardIndex when reaching end of flashcardsData
     }
+    showCardFront(currentCardIndex);
+}
 
-    function showCardFront() {
-        if(data && data[currentCard]) {
-            cardFront.find('p').text(data[currentCard].Front);
-            card.removeClass('flipped');
-        } else {
-            console.log("Error: data is not available or currentCard index is out of range.");
-        }
+function showPreviousCard() {
+    if (currentCardIndex > 0) {
+        currentCardIndex--; // Decrement currentCardIndex when showing previous card
+    } else {
+        currentCardIndex = flashcardsData.length - 1; // Set currentCardIndex to last index when reaching start of flashcardsData
     }
+    showCardFront(currentCardIndex);
+}
 
-    function showCardBack() {
-        console.log("currentCard:", currentCard); 
-    
-        if (currentCard && currentCard['Back'] && currentCard['Back'].trim().length > 0) {
-            flashcardContainer.innerHTML = currentCard['Back'];
-        } else {
-            throw new Error("Back data is not available for currentCard.");
-        }
-    }
+// Event listeners
+showAnswerButton.addEventListener('click', showCardBack);
+previousButton.addEventListener('click', showPreviousCard);
+nextButton.addEventListener('click', showNextCard);
 
-    show.addEventListener('click', function() {
-        if (!card.hasClass('flipped')) {
-            showCardBack();
-            card.addClass('flipped');
-        }
-    });
-
-    next.addEventListener('click', function() {
-        if (currentCard < data.length - 1) {
-            currentCard++;
-            showCardFront();
-        }
-    });
-
-    prev.addEventListener('click', function() {
-        if (currentCard > 0) {
-            currentCard--;
-            showCardFront();
-        }
-    });
-
-    getData();
+// Load flashcards data and show first card front
+$.get('./data.csv', function(data) {
+    var results = Papa.parse(data, {header: true});
+    flashcardsData = results['data'];
+    flashcardsData = shuffle(flashcardsData);
+    showCardFront(currentCardIndex);
 });
